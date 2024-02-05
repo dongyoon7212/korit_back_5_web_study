bff3bf3497d947a6d1ae1f8d2494e778042c3492
dongyoon7212 <dongyoon7212@naver.com>
2024-02-05 16:49:13 +0900

    javascript dom 객체

---
+++ javascript_study/연산자.js
@@ -0,0 +1,12 @@
+// 산술연산자
+console.log(10 + 20);
+console.log(40 - 20);
+console.log(10 * 2);
+console.log(10 / 2);
+console.log(10 % 3);
+
+// 비교연산자
+console.log(10 == "10");   // 타입비교 X
+console.log(10 === "10");  // 타입비교 O
+console.log(10 != "10");
+console.log(10 !== "10");
