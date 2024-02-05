8bc958f958e22cddd6b5cdaafa98bacadf170d45
dongyoon7212 <dongyoon7212@naver.com>
2024-02-05 14:36:25 +0900

    javascript

---
+++ javascript_study/hello.js
@@ -0,0 +1,22 @@
+// 자바스크립트
+
+//베이스 문법 -> 자바
+//타입리스
+
+var num = 10;
+num = "안녕";
+console.log(num);
+
+// typeof 키워드
+
+console.log(typeof 10); // 숫자 자료형 number
+console.log(typeof "10"); // 문자 string
+console.log(typeof true);
+
+var n;
+console.log(typeof n);
+console.log(n);
+n = null;
+console.log(typeof n);
+console.log(n);
+console.log(() => {})
