a24275f212b52e9f328ee7394f1b0b111d19e20d
dongyoon7212 <dongyoon7212@naver.com>
2024-02-05 17:53:47 +0900

    javascript

---
+++ javascript_study/js/dom3.js
@@ -0,0 +1,20 @@
+const addBtn = document.querySelector(".add-btn");
+let count = 1;
+
+addBtn.onclick = () => {
+    const inputName = document.querySelector("#input-name");
+    let inputNameValue = inputName.value;
+    const inputAge = document.querySelector("#input-age");
+    let inputAgeValue = inputAge.value;
+    const inputAddress = document.querySelector("#input-address");
+    let inputAddressValue = inputAddress.value;
+
+    const table = document.querySelector(".table");
+
+    table.innerHTML += `<tr><th>${count}</th><th>${inputNameValue}</th><th>${inputAgeValue}</th><th>${inputAddressValue}</th></tr>`;
+    count++;
+
+    inputName.value = "";
+    inputAge.value = "";
+    inputAddress.value = "";
+};
