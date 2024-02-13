eac5444ea316dc8f514523ce5d65ce4639b1da97
dongyoon7212 <dongyoon7212@naver.com>
2024-02-13 17:08:02 +0900

    front back 연동

--- insert_and_select/js/insert.js
+++ insert_and_select/js/insert.js
@@ -20,7 +20,16 @@ function handleSubmitClick() {
         body: jsonData,
     };
 
-    fetch("http://localhost:8080//insert_and_select/data/addition", option);
+    fetch(
+        "http://localhost:8080//insert_and_select/data/addition",
+        option
+    ).then((response) => {
+        response.json().then((json) => {
+            console.log(json.data);
+        });
+    }); // then 안에는 함수가 들어간다.
+
+    console.log("test");
 
     // const dataObj = JSON.parse(jsonData); // 다시 객체 형태로 변환
 
