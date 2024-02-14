ab9b645898fe312855d8b90029242f453d197234
dongyoon7212 <dongyoon7212@naver.com>
2024-02-14 12:13:47 +0900

    async, await

--- insert_and_select/js/insert.js
+++ insert_and_select/js/insert.js
@@ -20,24 +20,44 @@ async function handleSubmitClick() {
         body: jsonData,
     };
 
-    const response = await fetch(
-        "http://localhost:8080//insert_and_select/data/addition",
-        option
-    );
-    // .then((response) => {
-    //     response.json().then((json) => {
-    //         console.log(json.data);
-    //         console.log("test");
+    // fetch("http://localhost:8080//insert_and_select/data/addition", option)
+    //     .then((response) => {
+    //         response.json().then((json) => {
+    //             console.log(json.data);
+    //             console.log("test");
+    //         });
+    //     })
+    //     .catch((error) => {
+    //         console.log("프로미스 예외처리");
+    //         console.log(error);
     //     });
-    // }); // then 안에는 함수가 들어간다.
+    // then 안에는 함수가 들어간다.
     // 위는 promise로 사용할 경우
 
     // async await사용할 경우
-    const json = await response.json();
+    // await을 쓸땐 try catch를 사용해야한다.
+    try {
+        const response = await fetch(
+            "http://localhost:8080//insert_and_select/data/addition",
+            option
+        );
 
-    console.log(json);
+        if (!response.ok) {
+            throw await response.json();
+        }
 
-    console.log("test");
+        console.log(response);
+
+        const json = await response.json();
+
+        console.log(json);
+
+        console.log("test");
+    } catch (error) {
+        // console.log("에러 처리");
+        // console.log(error);
+        alert(error.errorMessage);
+    }
 
     // const dataObj = JSON.parse(jsonData); // 다시 객체 형태로 변환
 
